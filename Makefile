3830ecde87089a88f2dcbea6d799d24b3049f3d1
Prannay Khosla <prannay.khosla@gmail.com>
2017-10-04 23:24:46 +0530

    Completed code

---
+++ Makefile
@@ -0,0 +1,3 @@
+random :
+	g++ -I/home/prannayk/leda/incl random_linear.cpp
+	g++ -L/home/prannayk/leda random_linear.o -lX11 -lm -lleda
--- random_linear.cpp
+++ random_linear.cpp
@@ -3,33 +3,36 @@
 #include <LEDA/geo/point.h>
 #include <LEDA/geo/line.h>
 
-using namepsace std;
+using namespace std;
 using leda::point;
 using leda::circle;
 using leda::line ;
 
 int main(){
-	int n,i,j;
+	int n;
+	float iin, jin;
 	cin>>n;
 	point ps[n];
 	point p1;
 	int size = n;
 	while(n > 0){
 		n--;
-		cin>>i ; cin>>j;
-		point p1 (i,j);
+		cin>>iin ; cin>>jin;
+		point p1 (iin,jin);
 		ps[n] = p1;  
 	}
 	circle c;
 	if (size  == 1) {
 		cout << "Center of circle : " << ps[0] << " and radius is : " << 0 << endl;
-		return ; 
+		return 0; 
 	} else {
 		point p1((ps[0].to_vector() + ps[1].to_vector()) / 2);
 		circle c1(p1,ps[0].distance(p1));
+		c = c1;
 	}
 	int i = 2;
 	while ( i < size ) {
+		cout << i << endl ;
 		if (c.outside(ps[i])){
 			point center((ps[i].to_vector() + ps[0].to_vector()) / 2);
 			circle c2(center,center.distance(ps[0]));
